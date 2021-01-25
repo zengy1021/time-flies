5d2d2b8621d6dec829db9c56727785dd3eea8883
18671561774@163.com <18671561774@163.com>
2021-01-25 16:42:57 +0800

    添加elementui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-button>ok</el-button>
   </div>
 </template>
 
